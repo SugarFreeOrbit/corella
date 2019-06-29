f4d791050b5991d1848c9073b2188ad6d96b64f0
danilgrzh <danil.grzhybovskyy@dimatech.org>
2019-06-29 10:31:45 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());

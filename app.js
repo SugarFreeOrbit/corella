21efe9a20c6da10aabb7cb9375e9173013a07f84
danilgrzh <danil.grzhybovskyy@dimatech.org>
2019-07-01 12:36:24 +0300

    JWT signing and external configuration done.

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
+//Load main configuration file
+global.CONFIG = require('./configuration');
+//Require npm dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const passport = require('passport');
+
+//Load miscellaneous utils
+const responseMiddleware = require('./utils/responseMiddleware');
+
+//Load and export the logger configuration
 const logger = require('./utils/loggerConfig');
 global.logger = logger;
 
+//Initialize the express instance
 const app = express();
 
 //Init global db connection
-const dbUser = 'corellaAdmin';
-const dbPwd = 'bc5a6b33b659af8e426cdd4a141beaf33';
-const dbHost = '63.142.250.14';
-const dbName = 'corella';
+const dbUser = CONFIG.mongodb.user;
+const dbPwd = CONFIG.mongodb.pwd;
+const dbHost = CONFIG.mongodb.host;
+const dbName = CONFIG.mongodb.dbName;
 mongoose.connect(`mongodb://${dbUser}:${dbPwd}@${dbHost}/${dbName}`).catch(err => {
 	logger.error('Failed to connect to db!');
 }).then(() => {
 	logger.log('info', 'Connected to database!');
 });
 
+//Require and attach JWT parsing middleware
+const jwtStrategy = require('./utils/jwtStrategy');
+passport.use(jwtStrategy);
+
+//Initialize route
 const usersRouter = require('./routes/users');
 
+//Add middleware and start the HTTP listener
 app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan("combined", { stream: logger.stream }));
+app.use(responseMiddleware.defaultResponseContentType());
 app.use('/users', usersRouter);
 app.listen(8080);

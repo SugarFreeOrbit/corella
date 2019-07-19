3509d356f67c1381116f3bccc43a8e3fa5f277dd
danilgrzh <danil.grzhybovskyy@dimatech.org>
2019-07-19 14:09:29 +0300

    Projects are now being created

--- routes/projects.js
+++ routes/projects.js
@@ -3,10 +3,25 @@ const passport = require('passport');
 const validator = require('../utils/validation/validator');
 const Project = require('../models/project');
 
-router.put('/', [passport.authenticate('jwt', {session: false}), validator.check('newProfile')],  function (req, res) {
+router.put('/', [passport.authenticate('jwt', {session: false}), validator.check('newProject')],  function (req, res) {
 	if(req.user.isAdmin) {
-		res.status(200);
-		res.end();
+		let newProject = new Project({
+			name: req.body.name,
+			roles: req.body.roles,
+			columns: req.body.columns
+		});
+		newProject.save().then(() => {
+			res.status(201);
+			res.end();
+		}).catch((err) => {
+			logger.debug(err.toString());
+			if (err.name === 'ValidationError'){
+				res.status(400);
+			} else {
+				res.status(500);
+			}
+			res.json(err.message);
+		});
 	} else {
 		res.status(403);
 		res.end();

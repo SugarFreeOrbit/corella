a158c7f29e6d823e3c775ee8c11c36772821f01b
danilgrzh <danil.grzhybovskyy@dimatech.org>
2019-08-22 16:49:31 +0300

    Embedded issues into columns

--- routes/issues.js
+++ routes/issues.js
@@ -1,34 +1,32 @@
-const router = require('express').Router();
-const passport = require('passport');
-const validator = require('../utils/validation/validator');
-const Project = require('../models/project');
-const Issue = require('../models/issue');
-
-router.put('/', [validator.checkBody('newIssue')], async function (req, res, next) {
-	try {
-		if(await Project.checkCreatorPermission(req.body.project, req.user._id) || req.user.isAdmin) {
-			let newIssue = new Issue({
-				title: req.body.title,
-				description: req.body.description,
-				checklist: req.body.checklist
-			});
-			let appendIssueToProject = Project.findOneAndUpdate({
-				_id: req.body.project,
-				"columns.isStarting": true
-			}, {
-				$push: {
-					"columns.$.issues": newIssue._id
-				}
-			});
-			await Promise.all([newIssue.save(), appendIssueToProject]);
-			res.status(201);
-			res.end();
-		} else {
-			res.status(403);
-			res.end();
-		}
-	} catch (e) {
-		next(e);
-	}
-});
-module.exports = router;
+// const router = require('express').Router();
+// const validator = require('../utils/validation/validator');
+// const Project = require('../models/project');
+// // const Issue = require('../models/issue');
+//
+// router.put('/', [validator.checkBody('newIssue')], async function (req, res, next) {
+// 	try {
+// 		if(await Project.checkCreatorPermission(req.body.project, req.user._id) || req.user.isAdmin) {
+// 			let newIssue = {
+// 				title: req.body.title,
+// 				description: req.body.description,
+// 				checklist: req.body.checklist
+// 			};
+// 			await Project.findOneAndUpdate({
+// 				_id: req.body.project,
+// 				"columns.isStarting": true
+// 			}, {
+// 				$push: {
+// 					"columns.$.issues": newIssue._id
+// 				}
+// 			});
+// 			res.status(201);
+// 			res.end();
+// 		} else {
+// 			res.status(403);
+// 			res.end();
+// 		}
+// 	} catch (e) {
+// 		next(e);
+// 	}
+// });
+// module.exports = router;

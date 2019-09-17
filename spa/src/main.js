c57236433419dc0cfa42f4d87cd08f1f1dccda5c
danilgrzh <danil.grzhybovskyy@dimatech.org>
2019-09-17 16:25:13 +0300

    Static login page is ready

--- spa/src/main.js
+++ spa/src/main.js
@@ -1,10 +1,45 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios';
+import ElementUI from 'element-ui';
+import { LayoutPlugin } from 'bootstrap-vue'
+import 'element-ui/lib/theme-chalk/index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-Vue.config.productionTip = false
+Vue.use(axios);
+Vue.use(ElementUI);
+Vue.use(LayoutPlugin);
 
+axios.defaults.baseURL = process.env.VUE_APP_BACKEND_HOST;
+axios.defaults.headers.common['Content-Type'] = 'application/json';
+axios.interceptors.request.use( function (config) {
+	if (localStorage.getItem('jwt')) {
+		const token = localStorage.getItem('jwt');
+		config.headers.common['Authorization'] = `Bearer ${token}`;
+	}
+	return config;
+});
+axios.interceptors.response.use(undefined, (error) => {
+	if (error.request.status ===  0 || error.response.status === 401) {
+		localStorage.removeItem('jwt');
+		localStorage.removeItem('username');
+		localStorage.removeItem('userId');
+		localStorage.removeItem('apiKey');
+		router.push({ path: '/spa/login'});
+	}
+	return Promise.reject(error);
+});
+Vue.prototype.$http = axios;
+
+// set language to EN
+import lang from 'element-ui/lib/locale/lang/en';
+import locale from 'element-ui/lib/locale';
+locale.use(lang);
+
+Vue.config.productionTip = false;
 new Vue({
-  router,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+	router,
+	render: function (h) { return h(App) }
+}).$mount('#app');
---
+++ spa/src/views/Login.vue
@@ -0,0 +1,70 @@
+<template>
+	<div class="justify-content-center page-body-wrapper full-page-wrapper login">
+		<b-row class="h-100" align-v="center">
+			<b-col>
+				<div class="login__form">
+					<div class="login__form__logo"> </div>
+					<el-form :model="loginForm">
+						<el-form-item label="Username">
+							<el-input v-model="loginForm.username"></el-input>
+						</el-form-item>
+						<el-form-item label="Password">
+							<el-input v-model="loginForm.password" show-password></el-input>
+						</el-form-item>
+						<el-form-item>
+							<el-button type="primary">Log in</el-button>
+						</el-form-item>
+					</el-form>
+				</div>
+			</b-col>
+		</b-row>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "Login.vue",
+		data() {
+			return {
+				loginForm: {
+					username: "",
+					password: ""
+				}
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.login__form {
+		width: 300px;
+		font-weight: bold;
+		color: black;
+	}
+	.login {
+		display: flex;
+		height: 100%;
+		background-color: #87A330;
+		width: 100%;
+		text-align: center;
+	}
+	.login__form__logo {
+		background-image: url("../assets/birb.svg");
+		background-size: 200% 200%;
+		background-position: center;
+		margin-left: auto;
+		margin-right: auto;
+		width: 200px;
+		height: 200px;
+		z-index: 10000;
+	}
+	.el-button {
+		background-color: #FFE059;
+		color: black;
+		font-weight: bold;
+		border: none;
+	}
+	.el-button:hover {
+		background-color: #cfb24f;
+	}
+</style>

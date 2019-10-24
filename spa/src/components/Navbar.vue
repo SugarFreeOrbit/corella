cb8707001badb8bbf81d020da649f9d774b61618
danilgrzh <danil.grzhybovskyy@dimatech.org>
2019-10-24 16:34:13 +0300

    Add user modal + navbar update

--- spa/src/components/Navbar.vue
+++ spa/src/components/Navbar.vue
@@ -1,20 +1,22 @@
 <template>
 	<div class="navbar">
-		<div class="navbar__logo">
+		<div class="navbar__main">
+			<div class="navbar__logo">
+			</div>
+			<div class="navbar__menu">
+				<ul>
+					<li><router-link to="/">Projects</router-link></li>
+					<li v-if="userIsAdmin"><router-link to="/user-management">User management</router-link></li>
+				</ul>
+			</div>
 		</div>
-		<div class="navbar__menu">
-			<ul>
-				<li><router-link to="/">Projects</router-link></li>
-				<li v-if="userIsAdmin"><router-link to="/user-management">User management</router-link></li>
-			</ul>
-		</div>
-		<div class="navbar__currentUser">
-			Logged in as: {{username}}
-		</div>
-		<button class="navbar__logOut" @click="logout">
-			Log out
-		</button>
-		<div>
+		<div class="navbar__tools">
+			<div class="navbar__currentUser">
+				Logged in as: {{username}}
+			</div>
+			<button class="navbar__logOut" @click="logout">
+				Log out
+			</button>
 		</div>
 	</div>
 </template>
@@ -61,6 +63,7 @@
 	}
 	.navbar {
 		padding: 0;
+		box-sizing: border-box;
 		background-color: #87A330;
 		text-decoration: none;
 		font-weight: bold;
@@ -73,11 +76,8 @@
 			background-size: 200% 200%;
 			background-position: center;
 		}
-		&__menu {
-			float: left;
-		}
 		&__currentUser {
-			float: right;
+			float: left;
 			text-align: center;
 			padding: 14px 16px;
 		}
@@ -95,5 +95,17 @@
 				background-color: #c6a952;
 			}
 		}
+	}
+	.navbar__main {
+		display: flex;
+		flex-wrap: wrap;
+		flex-direction: row;
+		justify-content: flex-start;
+		position: relative;
+		float: left;
+	}
+	.navbar__tools {
+		position: relative;
+		float: left;
 	}
 </style>

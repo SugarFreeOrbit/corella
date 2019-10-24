6109cf79092f904b0257a2e3738cacc64401c0b8
danilgrzh <danil.grzhybovskyy@dimatech.org>
2019-10-24 16:34:13 +0300

    Add user modal + navbar update

--- spa/src/views/UserManagement.vue
+++ spa/src/views/UserManagement.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<navbar></navbar>
-		<div class="userManagement container" v-loading="loading">
+		<div class="userManagement container-fluid" v-loading="loading">
 			<el-dialog title="Edit user" center :visible.sync="editModalVisible" width="40%">
 				<el-form v-model="edit">
 					<el-form-item label="Username">
@@ -22,6 +22,26 @@
 					</el-form-item>
 				</el-form>
 			</el-dialog>
+			<el-dialog title="Add user" center :visible.sync="addModalVisible" width="40%">
+				<el-form v-model="add">
+					<el-form-item label="Username">
+						<el-input v-model="add.username"></el-input>
+					</el-form-item>
+					<el-form-item label="E-mail">
+						<el-input v-model="add.email"></el-input>
+					</el-form-item>
+					<el-form-item label="Password">
+						<el-input v-model="add.password" show-password></el-input>
+					</el-form-item>
+					<el-form-item label="Admin">
+						<el-switch v-model="add.isAdmin"></el-switch>
+					</el-form-item>
+					<el-form-item style="text-align: center;">
+						<el-button @click="addModalVisible = false">Cancel</el-button>
+						<el-button type="primary" @click="submitAddModal">Confirm</el-button>
+					</el-form-item>
+				</el-form>
+			</el-dialog>
 			<data-tables-server :data="users" :total="total" @query-change="handleQueryChange" :pagination-props="{ pageSizes: [10, 15, 20] }">
 				<el-table-column prop="username" label="Username"></el-table-column>
 				<el-table-column prop="email" label="E-mail"></el-table-column>
@@ -30,7 +50,10 @@
 						<el-switch v-model="props.row.isAdmin" @change="changeRole(props.row._id, props.row.isAdmin)"/>
 					</template>
 				</el-table-column>
-				<el-table-column label="Actions" custom width="auto">
+				<el-table-column custom width="auto">
+					<template slot="header" slot-scope="scope">
+						<el-button type="primary" round icon="el-icon-plus" @click="addModalVisible = true">Add user</el-button>
+					</template>
 					<template slot-scope="props">
 						<el-button type="danger" icon="el-icon-delete" @click="deleteUser(props.row._id)" circle></el-button>
 						<el-button type="primary" icon="el-icon-edit" circle @click="showEditModal(props.row)"></el-button>
@@ -55,12 +78,19 @@
 				pageCount: 0,
 				loading: false,
 				editModalVisible: false,
+				addModalVisible: false,
 				edit: {
 					username: '',
 					email: '',
 					password: '',
-					isAdmin: '',
+					isAdmin: false,
 					_id: ''
+				},
+				add: {
+					username: '',
+					email: '',
+					password: '',
+					isAdmin: false
 				}
 			}
 		},
@@ -118,6 +148,22 @@
 					await this.$http.patch(`/users/${this.edit._id}`, update);
 					this.loadUsers();
 					this.editModalVisible = false;
+				} catch (e) {
+					console.log(e);
+					throw e;
+				}
+			},
+			submitAddModal: async function() {
+				let newUser = {
+					username: this.add.username,
+					isAdmin: this.add.isAdmin,
+					password: this.add.password,
+					email: this.add.email
+				};
+				try {
+					await this.$http.put('/users', newUser);
+					this.loadUsers();
+					this.addModalVisible = false;
 				} catch (e) {
 					console.log(e);
 					throw e;

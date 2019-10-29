b14fd43d7c4f62d1e3060f2484210846fb830099
danilgrzh <danil.grzhybovskyy@dimatech.org>
2019-10-29 11:04:20 +0300

    Project builder step 1

--- spa/src/views/Projects.vue
+++ spa/src/views/Projects.vue
@@ -1,9 +1,30 @@
 <template>
 	<div style="height: 100%">
 		<navbar></navbar>
-		<div class="projects" v-loading="loading">
+		<div v-if="projectBuilder.visible" class="projectBuilder">
+			<div class="projectBuilder__close" @click="projectBuilder.visible = false"><i class="el-icon-close"></i></div>
+			<div class="projectBuilder__content container">
+				<el-steps :active="projectBuilder.step" align-center finish-status="success">
+					<el-step title="Name your project"></el-step>
+					<el-step title="Add columns"></el-step>
+					<el-step title="Add roles"></el-step>
+				</el-steps>
+				<el-form :model="projectBuilder.naming" class="projectBuilder__content__naming container" v-if="projectBuilder.step === 0">
+					<el-form-item label="Project name">
+						<el-input v-model="projectBuilder.naming.name"></el-input>
+					</el-form-item>
+					<el-form-item label="Project description">
+						<el-input v-model="projectBuilder.naming.description"></el-input>
+					</el-form-item>
+					<el-form-item style="text-align: center">
+						<el-button type="primary" @click="processNaming">Next</el-button>
+					</el-form-item>
+				</el-form>
+			</div>
+		</div>
+		<div class="projects" v-loading="loading" v-else>
 			<div class="projects__tools">
-				<el-button round icon="el-icon-plus" type="primary">Create new project</el-button>
+				<el-button round icon="el-icon-plus" type="primary" @click="projectBuilder.visible = true">Create new project</el-button>
 			</div>
 			<el-card v-for="project in projects" class="project" v-bind:key="project._id">
 				<div slot="header" class="clearfix">
@@ -25,7 +46,15 @@
 		data() {
 			return {
 				projects: [],
-				loading: true
+				loading: true,
+				projectBuilder: {
+					visible: false,
+					step: 0,
+					naming: {
+						name: '',
+						description: ''
+					}
+				}
 			}
 		},
 		methods: {
@@ -33,6 +62,9 @@
 				let res = await this.$http.get('/projects');
 				this.projects = res.data;
 				this.loading = false;
+			},
+			processNaming: async function() {
+				this.projectBuilder.step++;
 			}
 		},
 		mounted() {
@@ -64,6 +96,32 @@
 			}
 			&:active {
 				color: #87A330;
+			}
+		}
+	}
+	.projectBuilder {
+		&__close {
+			font-size: 25px;
+			position: relative;
+			top: 15px;
+			left: 15px;
+			cursor: pointer;
+			display: inline-block;
+			box-sizing: border-box;
+			padding: 2px;
+			&:hover {
+				color: #87A330;
+			}
+		}
+		&__content {
+			display: flex;
+			justify-content: space-between;
+			flex-direction: column;
+			flex-wrap: wrap;
+			width: 60%;
+			&__naming {
+				width: 60%;
+				margin-top: 100px;
 			}
 		}
 	}

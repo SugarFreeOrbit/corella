c57d1413e99f673235671f69d1726cd6d82eafe8
Bogdan Khlyan <es.bogdan.hlyan@gmail.com>
2020-08-04 14:55:25 +0300

    fix add issue modal

---
+++ spa/src/components/modals/AddIssueModal.vue
@@ -0,0 +1,119 @@
+<template>
+    <el-dialog :visible.sync="issueCreationModal.active" title="New issue">
+        <el-form v-model="issueCreationModal.form"
+                 v-loading="issueCreationModal.inProgress">
+            <el-form-item label="Title">
+                <el-input required v-model="issueCreationModal.form.title"></el-input>
+            </el-form-item>
+            <el-form-item label="Description">
+                <el-input type="textarea" v-model="issueCreationModal.form.description" :rows="5"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button @click="chooseFiles()" size="small" type="primary">Click to upload</el-button>
+                <input style="display: none" placeholder="upload files"
+                       type="file" id="uploadFiles" ref="files"
+                       multiple v-on:change="handleFilesUpload()" hidden/>
+                <div class="el-upload__tip">jpg/png files with a size less than 500kb</div>
+                <div v-if="issueCreationModal.form.files.length !== 0" class="modal__upload-wrapper">
+                    <ul class="modal__upload-list">
+                        <li v-for="(file, i) in issueCreationModal.form.files">
+                            <p class="name">{{file.name}}
+                                <span class="remove" @click='removeFile(file, i)'><i
+                                        class="el-icon-circle-close"></i></span>
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="createIssue">Create</el-button>
+                <el-button @click="issueCreationModal.active = false">Cancel</el-button>
+            </el-form-item>
+        </el-form>
+    </el-dialog>
+</template>
+
+
+<script>
+
+    export default {
+        name: "add-issue-modal",
+        components: {
+
+        },
+        props: {
+            name: String,
+            _id: String
+        },
+        data() {
+            return {
+                activeMenuItem: 'board',
+                issueCreationModal: {
+                    active: true,
+                    inProgress: false,
+                    form: {
+                        title: '',
+                        description: '',
+                        files: [],
+                        limitOfFiles: 3
+                    }
+                },
+            }
+        },
+        methods: {
+            createIssue: async function () {
+                let formData = new FormData();
+                this.issueCreationModal.inProgress = true;
+                formData.append('title', this.issueCreationModal.form.title);
+                formData.append('description', this.issueCreationModal.form.description);
+                this.issueCreationModal.form.files.forEach((file, i) => {
+                    formData.append('files', file);
+                });
+                if (this.$schemaValidators.validateNewIssue(this.issueCreationModal.form)) {
+                    let result = await this.$http.put(`/projects/${this._id}/issues`,
+                        formData,
+                        {
+                            headers: { 'Content-Type': 'multipart/form-data' }
+                        });
+                    this.issueCreationModal.inProgress = false;
+                    this.issueCreationModal.active = false;
+                    this.issueCreationModal.title = '';
+                    this.issueCreationModal.description = '';
+                } else {
+                    this.$notify({
+                        title: 'Error',
+                        message: 'Your issue is invalid',
+                        duration: 3000,
+                        type: 'error'
+                    });
+                    this.issueCreationModal.inProgress = false;
+                }
+            },
+            chooseFiles: function () {
+                document.getElementById("uploadFiles").click()
+            },
+            handleFilesUpload() {
+                let obj = this.$refs.files.files;
+                if (this.issueCreationModal.form.files.length !== this.issueCreationModal.form.limitOfFiles) {
+                    this.issueCreationModal.form.files.push(...obj);
+                } else {
+                    this.$notify({
+                        title: 'Error',
+                        message: 'To mach files',
+                        duration: 3000,
+                        type: 'error'
+                    });
+                }
+            },
+            removeFile: function (file, i) {
+                if (i > -1) {
+                    this.issueCreationModal.form.files.splice(i, 1);
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- spa/src/views/Project.vue
+++ spa/src/views/Project.vue
@@ -30,7 +30,7 @@
                         <i class="el-icon-plus"></i>
                     </div>
                     <div class="project__menu__item_create">
-                        <div class="project__menu__item_create__option" @click="issueCreationModal.active = true">
+                        <div class="project__menu__item_create__option" @click="isAddIssueModal = true">
                             New Issue
                         </div>
                     </div>
@@ -44,38 +44,7 @@
             <Board v-if="activeMenuItem === 'board'"></Board>
             <RolesAndMembers v-if="activeMenuItem === 'roles'"></RolesAndMembers>
             <Hotfixes v-if="activeMenuItem === 'hotfixes'"></Hotfixes>
-            <el-dialog :visible.sync="issueCreationModal.active" title="New issue">
-                <el-form v-model="issueCreationModal.form"
-                         v-loading="issueCreationModal.inProgress">
-                    <el-form-item label="Title">
-                        <el-input required v-model="issueCreationModal.form.title"></el-input>
-                    </el-form-item>
-                    <el-form-item label="Description">
-                        <el-input type="textarea" v-model="issueCreationModal.form.description" :rows="5"></el-input>
-                    </el-form-item>
-                    <el-form-item>
-                        <el-button @click="chooseFiles()" size="small" type="primary">Click to upload</el-button>
-                        <input style="display: none" placeholder="upload files"
-                               type="file" id="uploadFiles" ref="files"
-                               multiple v-on:change="handleFilesUpload()" hidden/>
-                        <div class="el-upload__tip">jpg/png files with a size less than 500kb</div>
-                        <div v-if="issueCreationModal.form.files.length != 0" class="modal__upload-wrapper">
-                            <ul class="modal__upload-list">
-                                <li v-for="(file, i) in issueCreationModal.form.files">
-                                    <p class="name">{{file.name}}
-                                        <span class="remove" @click='removeFile(file, i)'><i
-                                                class="el-icon-circle-close"></i></span>
-                                    </p>
-                                </li>
-                            </ul>
-                        </div>
-                    </el-form-item>
-                    <el-form-item>
-                        <el-button type="primary" @click="createIssue">Create</el-button>
-                        <el-button @click="issueCreationModal.active = false">Cancel</el-button>
-                    </el-form-item>
-                </el-form>
-            </el-dialog>
+            <add-issue-modal v-if="isAddIssueModal"></add-issue-modal>
         </div>
         <div v-else style="height: 100%" v-loading="projectReady"></div>
     </div>
@@ -87,9 +56,17 @@
     import RolesAndMembers from "../components/RolesAndMembers";
     import Hotfixes from "../components/Hotfixes";
 
+    import AddIssueModal from "../components/modals/AddIssueModal";
+
     export default {
         name: "Project",
-        components: {Hotfixes, RolesAndMembers, Board, Navbar},
+        components: {
+            Hotfixes,
+            RolesAndMembers,
+            Board,
+            Navbar,
+            AddIssueModal
+        },
         props: {
             name: String,
             _id: String
@@ -97,16 +74,7 @@
         data() {
             return {
                 activeMenuItem: 'board',
-                issueCreationModal: {
-                    active: false,
-                    inProgress: false,
-                    form: {
-                        title: '',
-                        description: '',
-                        files: [],
-                        limitOfFiles: 3
-                    }
-                },
+                isAddIssueModal: false,
                 projectReady: false
             }
         },
@@ -135,59 +103,6 @@
 
         },
         methods: {
-            createIssue: async function () {
-                let formData = new FormData();
-                this.issueCreationModal.inProgress = true;
-                formData.append('title', this.issueCreationModal.form.title);
-                formData.append('description', this.issueCreationModal.form.description);
-                for (let i = 0; i < this.issueCreationModal.form.files; i++) {
-                    let file = this.this.issueCreationModal.form.files[i];
-                    formData.append('files[' + i + ']', file);
-                }
-                if (this.$schemaValidators.validateNewIssue(this.issueCreationModal.form)) {
-                    let result = await this.$http.put(`/projects/${this._id}/issues`,
-                        formData,
-                        {
-                            headers: {
-                                'Content-Type': 'multipart/form-data'
-                            }
-                        });
-                    this.issueCreationModal.inProgress = false;
-                    this.issueCreationModal.active = false;
-                    this.issueCreationModal.title = '';
-                    this.issueCreationModal.description = '';
-                    console.log(result);
-                } else {
-                    this.$notify({
-                        title: 'Error',
-                        message: 'Your issue is invalid',
-                        duration: 3000,
-                        type: 'error'
-                    });
-                    this.issueCreationModal.inProgress = false;
-                }
-            },
-            chooseFiles: function () {
-                document.getElementById("uploadFiles").click()
-            },
-            handleFilesUpload() {
-                let obj = this.$refs.files.files;
-                if (this.issueCreationModal.form.files.length !== this.issueCreationModal.form.limitOfFiles) {
-                    this.issueCreationModal.form.files.push(...obj);
-                } else {
-                    this.$notify({
-                        title: 'Error',
-                        message: 'To mach files',
-                        duration: 3000,
-                        type: 'error'
-                    });
-                }
-            },
-            removeFile: function (file, i) {
-                if (i > -1) {
-                    this.issueCreationModal.form.files.splice(i, 1);
-                }
-            }
         }
     }
 </script>

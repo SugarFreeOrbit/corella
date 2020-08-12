8d0c99d053e176a8f8ed23c2aa01d480784787d9
danilgrzh <danil.grzhybovskyy@dimatech.org>
2020-08-12 11:17:11 +0300

    Migration update

--- migrations/from_past_to_1.0/migrate.js
+++ migrations/from_past_to_1.0/migrate.js
@@ -7,12 +7,21 @@ mongoose.set('useNewUrlParser', true);
 let CONFIG = require('../../configuration');
 let Project = require('../../models/project');
 let Issue = require('../../models/issue');
+let Counter = require('../../models/counter');
 let dbConnPromise = mongoose.connect(CONFIG.mongodb.connection);
 dbConnPromise.then(async db => {
+	 console.log("Connected to db");
 	 let projects = await Project.find({}, {columns: 1});
-	 projects.forEach(async project => {
-	 	project.columns.async.forEach(async issue => {
-	 		await Issue.updateOne({_id: issue}, {$set: {projectId: project._id, files: []}});
-		});
-	 });
+	 for (let i= 0; i < projects.length; i++) {
+		 for (let j = 0; j < projects[i].columns.length; j++) {
+		 	try {
+				let nextCode = await Counter.getNextSequenceCount();
+				await Issue.updateOne({_id: projects[i].columns[j]}, {$set: {projectId: projects[i]._id, files: [], issueCode: nextCode}});
+			} catch (e) {
+				console.log(`Woops, something went wrong! Issue id: ${projects[i].columns[j]}, project id: ${projects[i]._id}`);
+				console.log(e);
+			}
+		 }
+	 }
+	 console.log('Migration has finished');
 });
